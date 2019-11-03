39d2170b456a25169c4fce2f8df9f1137f01b59e
Paul Ermolin <oatto.paul@gmail.com>
2019-11-03 14:55:57 +0100

    refactor: use .yaml for content config

--- gatsby_apis/node/create_pages.js
+++ gatsby_apis/node/create_pages.js
@@ -1,8 +1,10 @@
 const fs = require('fs')
 const path = require('path')
-const locales = require(path.resolve(process.cwd(), 'src/i18n/locales.json'))
+const yaml = require('js-yaml')
 
+const locales = require(path.resolve(process.cwd(), 'src/i18n/locales.json'))
 const layouts = path.resolve(process.cwd(), `src/layouts`)
+
 const getLayout = (name) => path.join(layouts, `${name}.jsx`)
 
 const createPages = async ({ graphql, actions }) => {
@@ -36,10 +38,10 @@ const createPages = async ({ graphql, actions }) => {
     const parentDir = path.dirname(fileAbsolutePath)
     const fileName = path.basename(fileAbsolutePath, `.md`).replace(/_/g, `-`)
     const pathToImages = `${parentDir}/images`
-    const pathToConfig = path.join(parentDir, `customization.json`)
-    const config = fs.existsSync(pathToConfig) && JSON.parse(
-      fs.readFileSync(pathToConfig, `utf-8`)
-    )
+    const pathToConfig = path.join(parentDir, `customization.yaml`)
+    const config = fs.existsSync(pathToConfig) &&
+      yaml.safeLoad(fs.readFileSync(pathToConfig, `utf-8`))
+
     const slug = config
       ? config.base_path + (pathOverride || `/${fileName}/`)
       : fields.slug
