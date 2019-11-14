d1acbc5a2a507313207dd37022424ddda36e7323
Paul Ermolin <oatto.paul@gmail.com>
2019-11-14 20:43:51 +0100

    refactor(layouts): split developers page into components

--- src/layouts/developers.jsx
+++ src/layouts/developers.jsx
@@ -2,34 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 
-import { useLayoutContext } from '../context/layout'
-import Link from '../components/link'
+import { Hero, Thumb } from '../components/pages/developers'
 import { generateKey } from '../components/utils'
 
-export const DevelopersPage = ({ data }) => {
+const DevelopersPage = ({ data }) => {
   const { content } = data.mdx.frontmatter
-  const { forms } = useLayoutContext()
 
   return (
     <div className='index'>
-      <section className='section'>
-        <div className='section__in section__in_padding'>
-          <h3 className='section__title'>
-            {content.hero.title}
-          </h3>
-          <p className='section__description'>
-            {content.hero.description.paragraph_1}
-            <br />
-            <button
-              className='section__description-link section__description-link_black'
-              onClick={forms.contact.toggle}
-            >
-              {content.hero.description.paragraph_2.button}
-            </button>
-            {content.hero.description.paragraph_2.text}
-          </p>
-        </div>
-      </section>
+      <Hero {...content.hero} />
       <section className='section'>
         <div className={`
           section__in
@@ -37,24 +18,11 @@ export const DevelopersPage = ({ data }) => {
           section__in_reverse
         `}>
           <ul className='developers-thumbs'>
-            {content.thumbs.map(({ title, description, icon, to }, idx) => (
-              <li
-                key={generateKey(title, idx)}
-                className='developers-thumbs__item'
-              >
-                <Link
-                  className='developers-thumbs__link'
-                  to={to}
-                >
-                  <i className={`developers-thumbs__icon fa ${icon}`} />
-                  <span className='developers-thumbs__title'>
-                    {title}
-                  </span>
-                  <p className='developers-thumbs__description'>
-                    {description}
-                  </p>
-                </Link>
-              </li>
+            {content.thumbs.map((props, idx) => (
+              <Thumb
+                key={generateKey(props.title, idx)}
+                {...props}
+              />
             ))}
           </ul>
         </div>
@@ -102,7 +70,7 @@ DevelopersPage.propTypes = {
               paragraph_2: PropTypes.shape({
                 button: PropTypes.string.isRequired,
                 text: PropTypes.string.isRequired
-              })
+              }).isRequired
             }).isRequired
           }).isRequired,
           thumbs: PropTypes.arrayOf(
