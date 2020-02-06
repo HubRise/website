a2ec7353ea8b780d9e6bd5c46b641f8551233a43
Vital Ozierski <ozicoder@gmail.com>
2020-02-06 23:18:05 +0300

    Fix flickering and call-summary table

--- src/components/documentation/section_navigation.jsx
+++ src/components/documentation/section_navigation.jsx
@@ -76,96 +76,101 @@ export const SectionNavigation = ({ currentPath, pages, title, logo }) => {
         section__sidebar_small-padding
         ${logo ? 'section__sidebar_sticky' : ''}
       `}
-      ref={containerRef}
     >
       {logo && (
         <div className="section__sidebar_logo">
           <NonStretchedImage alt={logo.name} {...logo.childImageSharp} />
         </div>
       )}
-      <div
-        className={cx(
-          'section__sidebar-in',
-          logo && 'section__sidebar_sticky',
-          isFixed && 'section__sidebar_fixed'
-        )}
-      >
-        <h5 className="content-nav__title">{currentTitle || `Content`}</h5>
-        <h5
-          id="content-nav"
-          className={`
+      <div ref={containerRef} className="section__sidebar-in-wrapper">
+        <div
+          className={cx(
+            'section__sidebar-in',
+            logo && 'section__sidebar_sticky',
+            isFixed && 'section__sidebar_fixed'
+          )}
+        >
+          <h5 className="content-nav__title">{currentTitle || `Content`}</h5>
+          <h5
+            id="content-nav"
+            className={`
             content-nav__title
             content-nav__title_small
             ${isExpanded ? 'content-nav__title_small_bottom_border' : ''}
           `}
-          onClick={() => setIsExpanded(!isExpanded)}
-        >
-          {currentTitle || `Content`}
-          <i
-            className={`
+            onClick={() => setIsExpanded(!isExpanded)}
+          >
+            {currentTitle || `Content`}
+            <i
+              className={`
               fa
               fa-angle-${isExpanded ? 'up' : 'down'}
               content-nav__arrow
             `}
-          />
-        </h5>
-        <ul
-          id="content-nav-list"
-          className={`
+            />
+          </h5>
+          <ul
+            id="content-nav-list"
+            className={`
             content-nav__list
             ${isExpanded ? '' : 'content-nav__list_hidden'}
           `}
-        >
-          {sortPagesAsc(pages).map(({ frontmatter, fields, headings }, idx) => {
-            const { slug } = fields
-            const isCurrentPage = currentPath.endsWith(slug)
+          >
+            {sortPagesAsc(pages).map(
+              ({ frontmatter, fields, headings }, idx) => {
+                const { slug } = fields
+                const isCurrentPage = currentPath.endsWith(slug)
 
-            return (
-              <li
-                key={generateKey(frontmatter.title, idx)}
-                className={`content-nav__item ${
-                  isCurrentPage ? 'content-nav__item_active' : ''
-                }`}
-              >
-                <Link
-                  to={slug}
-                  className="content-nav__link"
-                  onClick={isDesktop ? undefined : () => setIsExpanded(false)}
-                >
-                  {frontmatter.title}
-                </Link>
-                {isCurrentPage && headings.length !== 0 && (
-                  <ol className="content-sublist">
-                    {headings
-                      .filter(({ depth }) => depth === 2)
-                      .map(({ value: headingText }, idx) => {
-                        return (
-                          <li
-                            key={generateKey(headingText, idx)}
-                            className="content-sublist-item content-sublist-level-2"
-                          >
-                            <Link
-                              className="content-sublist-link"
-                              to={`#${createHeaderAnchor(headingText)}`}
-                              onClick={
-                                isDesktop
-                                  ? undefined
-                                  : () => setIsExpanded(false)
-                              }
-                            >
-                              <span className="content-sublist-text">
-                                {headingText}
-                              </span>
-                            </Link>
-                          </li>
-                        )
-                      })}
-                  </ol>
-                )}
-              </li>
-            )
-          })}
-        </ul>
+                return (
+                  <li
+                    key={generateKey(frontmatter.title, idx)}
+                    className={`content-nav__item ${
+                      isCurrentPage ? 'content-nav__item_active' : ''
+                    }`}
+                  >
+                    <Link
+                      to={slug}
+                      className="content-nav__link"
+                      onClick={
+                        isDesktop ? undefined : () => setIsExpanded(false)
+                      }
+                    >
+                      {frontmatter.title}
+                    </Link>
+                    {isCurrentPage && headings.length !== 0 && (
+                      <ol className="content-sublist">
+                        {headings
+                          .filter(({ depth }) => depth === 2)
+                          .map(({ value: headingText }, idx) => {
+                            return (
+                              <li
+                                key={generateKey(headingText, idx)}
+                                className="content-sublist-item content-sublist-level-2"
+                              >
+                                <Link
+                                  className="content-sublist-link"
+                                  to={`#${createHeaderAnchor(headingText)}`}
+                                  onClick={
+                                    isDesktop
+                                      ? undefined
+                                      : () => setIsExpanded(false)
+                                  }
+                                >
+                                  <span className="content-sublist-text">
+                                    {headingText}
+                                  </span>
+                                </Link>
+                              </li>
+                            )
+                          })}
+                      </ol>
+                    )}
+                  </li>
+                )
+              }
+            )}
+          </ul>
+        </div>
       </div>
     </div>
   )
