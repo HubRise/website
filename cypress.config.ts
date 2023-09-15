81dc607f305ac32042f9b4ebfb321f12c971cff5
antmonnier <monniera@gmail.com>
2023-09-15 08:50:16 +0200

    [debug] taskTimeout: 10000

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
     defaultCommandTimeout: +process.env.CYPRESS_DEFAULT_COMMAND_TIMEOUT || 4000,
-    pageLoadTimeout: 10000,
+    taskTimeout: 10000,
     viewportWidth: 1280,
     viewportHeight: 720,
     setupNodeEvents(_on, _config) {
