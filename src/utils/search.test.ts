e06c0ec48ef7bdb56361d9006ce6f81965b138bf
Felix Lopez <felixlopzd@gmail.com>
2023-11-08 13:58:42 -0400

    adding accent test

--- src/utils/search.test.ts
+++ src/utils/search.test.ts
@@ -10,5 +10,6 @@ describe("filterSearch", () => {
   it("should tell if texts match", () => {
     expect(doesSearchTextMatch("T.A.L.C", "talc")).toBe(true)
     expect(doesSearchTextMatch("3S POS", "asd")).toBe(false)
+    expect(doesSearchTextMatch("Carré POS", "carre")).toBe(true)
   })
 })
