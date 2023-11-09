6a07e540abacb082f66330fa6c72cd06805853dd
antmonnier <monniera@gmail.com>
2023-11-09 15:29:13 +0100

    Review

--- src/utils/search.test.ts
+++ src/utils/search.test.ts
@@ -2,18 +2,26 @@ import { doesSearchTextMatch } from "./search"
 
 describe("filterSearch", () => {
   it("should ignore dots", () => {
-    expect(doesSearchTextMatch("T.A.L.C", "talc")).toBe(true)
+    expect(doesSearchTextMatch("T.A.L.C", "TALC")).toBe(true)
   })
+
+  it("should ignore case", () => {
+    expect(doesSearchTextMatch("3S POS", "3s pos")).toBe(true)
+  })
+
   it("should ignore accents", () => {
-    expect(doesSearchTextMatch("Carré", "carre")).toBe(true)
+    expect(doesSearchTextMatch("carré", "carre")).toBe(true)
   })
+
   it("should ignore spaces", () => {
-    expect(doesSearchTextMatch("3S POS", "3spos")).toBe(true)
+    expect(doesSearchTextMatch("3S POS", "3SPOS")).toBe(true)
   })
+
   it("should ignore special characters", () => {
-    expect(doesSearchTextMatch("K-Series/", "kseries")).toBe(true)
+    expect(doesSearchTextMatch("k-series/", "kseries")).toBe(true)
   })
-  it("should not find a match", () => {
+
+  it("not always return true", () => {
     expect(doesSearchTextMatch("3S POS", "qwerty")).toBe(false)
   })
 })
