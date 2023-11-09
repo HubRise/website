19e7ac19eba0c5f7788b8adedcdd26587ae531aa
Felix Lopez <felixlopzd@gmail.com>
2023-11-09 09:27:57 -0400

    removing redundant test

--- src/utils/search.test.ts
+++ src/utils/search.test.ts
@@ -1,12 +1,6 @@
-import { doesSearchTextMatch, normalizeText } from "./search"
+import { doesSearchTextMatch } from "./search"
 
 describe("filterSearch", () => {
-  it("should normalize string for search", () => {
-    expect(normalizeText("T.A.L.C")).toBe("talc")
-    expect(normalizeText("k-series")).toBe("kseries")
-    expect(normalizeText("3S POS")).toBe("3spos")
-  })
-
   it("should tell if texts match", () => {
     expect(doesSearchTextMatch("T.A.L.C", "talc")).toBe(true)
     expect(doesSearchTextMatch("3S POS", "asd")).toBe(false)
