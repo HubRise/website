e2515cd81edc05d5729b77ecdc17c8c7b24edef1
Anton Berezenskyi <ant.vla.ber@gmail.com>
2025-02-02 17:28:30 +0200

    Use <image> for hero apps arc image

--- src/layouts/Frontpage/Hero/utils.ts
+++ src/layouts/Frontpage/Hero/utils.ts
@@ -10,4 +10,11 @@ const linkHeroAppColorMap: HeroAppColorMap = {
   greenMediumLight: colors.greenMediumLight,
 }
 
+const heroAppsImageSourceMap: HeroAppColorMap = {
+  green: "/images/frontpage/hero-apps-top.svg",
+  greenLight: "/images/frontpage/hero-apps-bottom-light.svg",
+  greenMediumLight: "/images/frontpage/hero-apps-top-midlight.svg",
+}
+
 export const linkHeroAppBorderColor = (color: HeroAppColor) => linkHeroAppColorMap[color]
+export const getHeroAppsImageSource = (color: HeroAppColor) => heroAppsImageSourceMap[color]
