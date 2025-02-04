3ccadbdbda6c8e35685bcb4346f0ed94cbb2307b
Anton Berezenskyi <ant.vla.ber@gmail.com>
2025-02-04 19:28:39 +0200

    reusable button and integrations screen

--- src/components/Block/utils.ts
+++ src/components/Block/utils.ts
@@ -40,6 +40,6 @@ export const linkOverColorMap: ColorMap = {
 
 export const colorMap: ColorMap = {
   none: colors.textMedium,
-  white: colors.textMedium,
+  white: colors.textDark,
   green: colors.white,
 }
