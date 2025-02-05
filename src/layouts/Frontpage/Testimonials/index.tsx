ae7fd1bb3bc442b6cc056cb8ac04cabd4ffb73c5
Anton Berezenskyi <ant.vla.ber@gmail.com>
2025-02-05 16:01:49 +0200

    add testimonials screen

---
+++ src/layouts/Frontpage/Testimonials/index.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image"
+
+import Container from "@components/Container"
+import ContainerHeader from "@components/ContainerHeader"
+import { ContentImageMap } from "@utils/contentImage"
+
+import { Card } from "../shared/Styles"
+import { TTestimonial } from "../types"
+
+import { TestimonialsWrapper, Name, JobTitle, Text, LogoImage } from "./Styles"
+
+interface TestimonialsProps {
+  title: string
+  testimonials: Array<TTestimonial>
+  testimonialLogoMap: ContentImageMap
+}
+
+const Testimonials = ({ title, testimonials, testimonialLogoMap }: TestimonialsProps) => {
+  return (
+    <Container bgColor="backgroundLight" verticalPadding="big">
+      <ContainerHeader title={title} />
+      <TestimonialsWrapper>
+        {testimonials.map((testimonial, index) => {
+          console.log(testimonialLogoMap[testimonial.logo])
+          return (
+            <Card $padding="small" key={index}>
+              <Image src="/images/quote.svg" alt="Quote" width={62} height={44}></Image>
+              <Name>{`- ${testimonial.name}`}</Name>
+              <JobTitle>{testimonial.job_title}</JobTitle>
+              <Text>{testimonial.text}</Text>
+              {testimonialLogoMap[testimonial.logo] && (
+                <LogoImage
+                  {...testimonialLogoMap[testimonial.logo]}
+                  alt={testimonial.logo.substring(0, testimonial.logo.length - 4)}
+                ></LogoImage>
+              )}
+            </Card>
+          )
+        })}
+      </TestimonialsWrapper>
+    </Container>
+  )
+}
+
+export default Testimonials
