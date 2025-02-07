67b50db5bbdc77ec4f339af7f861a3154909ce91
Anton Berezenskyi <ant.vla.ber@gmail.com>
2025-02-07 09:38:51 +0200

    add SideBlock component

--- src/layouts/Frontpage/Integrations/index.tsx
+++ src/layouts/Frontpage/Integrations/index.tsx
@@ -1,15 +1,15 @@
 import Image from "next/image"
 
-import Block from "@components/Block"
 import Button from "@components/Button"
 import Container from "@components/Container"
 import ContainerHeader from "@components/ContainerHeader"
 import Icon from "@components/Icon"
+import { SideBlock } from "@components/SideBlock"
 import { iconSizes } from "@utils/styles"
 
 import { TIntegrationApp } from "../types"
 
-import { Advantage, Advantages, IconWrapper } from "./Styles"
+import { ContentWrapper, Advantage, Advantages, IconWrapper } from "./Styles"
 
 interface IntegrationsProps {
   title: string
@@ -22,39 +22,35 @@ const Integrations = ({ title, integration_apps }: IntegrationsProps) => {
       <ContainerHeader title={title} />
       {integration_apps.map((integrationApp, index) => {
         return (
-          <Block
+          <SideBlock
             key={index}
-            backgroundColor="white"
-            padding="small"
-            title={integrationApp.title}
-            beforeExpansion={!integrationApp.afterExpansion}
-            afterExpansion={integrationApp.afterExpansion}
-            horizontalAlign="centerOnMobile"
-            desktopVerticalAlign="center"
-            side={
+            side={index % 2 ? "left" : "right"}
+            secondaryContent={
               <Image src={`/images/frontpage/${integrationApp.image}`} alt="Documentation" width={915} height={750} />
             }
-            sidePosition={integrationApp.sidePosition}
+            secondaryPosition={index % 2 ? "left" : "right"}
           >
-            <Advantages>
-              {integrationApp.advantages.map(({ text }, key) => {
-                return (
-                  <Advantage key={key}>
-                    <IconWrapper>
-                      <Icon code="check" size={iconSizes._25} color="white" />
-                    </IconWrapper>
-                    <span>{text}</span>
-                  </Advantage>
-                )
-              })}
-            </Advantages>
-            <Button
-              label={integrationApp.button_label}
-              link={integrationApp.button_link}
-              type="secondary"
-              icon={<Icon code="arrow_forward" size={iconSizes._25} />}
-            />
-          </Block>
+            <ContentWrapper $padding={index % 2 ? "right" : "left"}>
+              <Advantages>
+                {integrationApp.advantages.map(({ text }, key) => {
+                  return (
+                    <Advantage key={key}>
+                      <IconWrapper>
+                        <Icon code="check" size={iconSizes._25} color="white" />
+                      </IconWrapper>
+                      <span>{text}</span>
+                    </Advantage>
+                  )
+                })}
+              </Advantages>
+              <Button
+                label={integrationApp.button_label}
+                link={integrationApp.button_link}
+                type="secondary"
+                icon={<Icon code="arrow_forward" size={iconSizes._25} />}
+              />
+            </ContentWrapper>
+          </SideBlock>
         )
       })}
     </Container>
