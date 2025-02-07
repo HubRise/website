3cfdbb6575cf6918631c8c17fcbd28966483385a
Anton Berezenskyi <ant.vla.ber@gmail.com>
2025-02-07 10:21:04 +0200

    move proposals images to content

--- src/layouts/Frontpage/Testimonials/index.tsx
+++ src/layouts/Frontpage/Testimonials/index.tsx
@@ -24,7 +24,7 @@ const Testimonials = ({ title, testimonials, testimonialLogoMap }: TestimonialsP
           console.log(testimonialLogoMap[testimonial.logo])
           return (
             <Card $padding="small" key={index}>
-              <Image src="/images/quote.svg" alt="Quote" width={62} height={44}></Image>
+              <Image src="/images/frontpage/quote.svg" alt="Quote" width={62} height={44}></Image>
               <Name>{`- ${testimonial.name}`}</Name>
               <JobTitle>{testimonial.job_title}</JobTitle>
               <Text>{testimonial.text}</Text>
