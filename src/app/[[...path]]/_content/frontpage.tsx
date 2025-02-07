d4f7250be014b03c035442d3c6db6cb48065116e
Anton Berezenskyi <ant.vla.ber@gmail.com>
2025-02-07 10:21:04 +0200

    move proposals images to content

--- src/app/[[...path]]/_content/frontpage.tsx
+++ src/app/[[...path]]/_content/frontpage.tsx
@@ -9,21 +9,30 @@ import { Route, RouteName } from "@utils/router/types"
 const frontpage = async (route: Route<RouteName, "frontpage">): Promise<JSX.Element> => {
   const yaml = route.context.yaml
   const testimonialLogos: string[] = yaml.content.testimonials.testimonials.map((testimonial) => testimonial.logo)
+  const proposalsImages: string[] = yaml.content.proposals.proposals_cards.map((proposal) => proposal.image)
 
-  const [heroDescriptionMdx, useDescriptionMdx, pricingDescriptionMdx, partnersDescriptionMdx, testimonialLogoMap] =
-    await Promise.all([
-      serializeFrontpage(yaml.hero.description),
-      serializeFrontpage(yaml.content.use.description),
-      serializeFrontpage(yaml.content.pricing.description),
-      serializeFrontpage(yaml.content.partners.description),
-      contentImageMap("/images/app-logos", testimonialLogos),
-    ])
+  const [
+    heroDescriptionMdx,
+    proposalsDescriptionMdx,
+    proposalsImagesMap,
+    pricingDescriptionMdx,
+    partnersDescriptionMdx,
+    testimonialLogoMap,
+  ] = await Promise.all([
+    serializeFrontpage(yaml.hero.description),
+    serializeFrontpage(yaml.content.proposals.description),
+    contentImageMap("/images/proposals", proposalsImages),
+    serializeFrontpage(yaml.content.pricing.description),
+    serializeFrontpage(yaml.content.partners.description),
+    contentImageMap("/images/app-logos", testimonialLogos),
+  ])
 
   return (
     <Frontpage
       yaml={yaml}
       heroDescriptionMdx={heroDescriptionMdx}
-      useDescriptionMdx={useDescriptionMdx}
+      proposalsDescriptionMdx={proposalsDescriptionMdx}
+      proposalsImagesMap={proposalsImagesMap}
       pricingDescriptionMdx={pricingDescriptionMdx}
       partnersDescriptionMdx={partnersDescriptionMdx}
       testimonialLogoMap={testimonialLogoMap}
