9980c488dc816f3558a469f8251f3b75bc37866e
Anton Berezenskyi <ant.vla.ber@gmail.com>
2025-02-11 11:30:18 +0200

    mobile version of home page

--- src/layouts/Frontpage/Pricing/Styles.ts
+++ src/layouts/Frontpage/Pricing/Styles.ts
@@ -1,8 +1,15 @@
 import styled from "styled-components"
 
+import { breakpoints } from "@utils/styles"
+
 export const ButtonWrapper = styled.div`
   & > a {
     margin-left: auto;
     margin-right: auto;
+    width: 100%;
+
+    @media (min-width: ${breakpoints.medium}) {
+      width: fit-content;
+    }
   }
 `
