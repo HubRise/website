886c1e09337832eb0bc54be4cb87b3ea4bbb8309
Anton Berezenskyi <ant.vla.ber@gmail.com>
2025-02-12 15:07:02 +0200

    home page CR: container component refactoring

--- src/layouts/Frontpage/Features/index.tsx
+++ src/layouts/Frontpage/Features/index.tsx
@@ -1,7 +1,6 @@
 import { type MDXRemoteSerializeResult } from "next-mdx-remote"
 
 import Container from "@components/Container"
-import ContainerHeader from "@components/ContainerHeader"
 import { ContentImageMap } from "@utils/contentImage"
 
 import { Card } from "../shared/Styles"
@@ -18,8 +17,7 @@ interface FeaturesProps {
 
 const Features = ({ title, features_cards, descriptionMdx, featuresImagesMap }: FeaturesProps): JSX.Element => {
   return (
-    <Container bgColor="backgroundLight" verticalPadding="big">
-      <ContainerHeader title={title} descriptionMdx={descriptionMdx} />
+    <Container bgColor="backgroundLight" verticalPadding="big" withHeader title={title} descriptionMdx={descriptionMdx}>
       <Cards>
         {features_cards.map(({ title, description, image }, index) => {
           return (
