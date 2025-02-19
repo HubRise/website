8b71cae65b36c899896cf0ef608ae12463c855a7
Anton Berezenskyi <ant.vla.ber@gmail.com>
2025-02-19 12:07:24 +0200

    pricing page

---
+++ src/layouts/Pricing/WhyChoose/index.tsx
@@ -0,0 +1,32 @@
+import Card from "@components/Card"
+import ScreenContainer from "@components/ScreenContainer"
+
+import { TWhyChoose } from "../types"
+
+import { Cards, CardNumber, CardTitle, CardText, Wrapper } from "./Styles"
+
+interface WhyChooseProps {
+  why_choose: TWhyChoose
+}
+
+const WhyChoose = ({ why_choose }: WhyChooseProps): JSX.Element => {
+  return (
+    <ScreenContainer bgColor="white" verticalPadding="big" withHeader title={why_choose.title}>
+      <Wrapper>
+        <Cards>
+          {why_choose.cards.map(({ title, text }, index) => {
+            return (
+              <Card key={index}>
+                <CardNumber>0{index + 1}</CardNumber>
+                <CardTitle>{title}</CardTitle>
+                <CardText>{text}</CardText>
+              </Card>
+            )
+          })}
+        </Cards>
+      </Wrapper>
+    </ScreenContainer>
+  )
+}
+
+export default WhyChoose
