627b421a36a448c8f57e1c9935d0492de57436b0
Anton Berezenskyi <ant.vla.ber@gmail.com>
2025-02-21 17:12:12 +0200

    revert custom google map api

--- src/layouts/ContactUs/Map/index.tsx
+++ src/layouts/ContactUs/Map/index.tsx
@@ -1,5 +1,5 @@
+import { Marker, Map as GoogleMap } from "@vis.gl/react-google-maps"
 import Image from "next/image"
-import { useLayoutEffect } from "react"
 
 import Card from "@components/Card"
 
@@ -16,35 +16,6 @@ interface MapProps {
 const Map = ({ map }: MapProps): JSX.Element => {
   const { title, description, image, address } = map
 
-  useLayoutEffect(() => {
-    let map
-    const google = (window as any)?.google
-
-    const initMap = async () => {
-      const position = { lat: 43.6214593381594, lng: 7.0392507747754625 }
-      const { Map } = await google.maps.importLibrary("maps")
-      const { AdvancedMarkerElement } = await google.maps.importLibrary("marker")
-
-      map = new Map(document.getElementById("map") as HTMLElement, {
-        zoom: 17,
-        center: position,
-        mapId: "contact-us-map",
-        disableDefaultUI: true,
-      })
-
-      new AdvancedMarkerElement({
-        map: map,
-        position: position,
-      })
-
-      const styledMapType = new google.maps.StyledMapType(mapStyles)
-      map.mapTypes.set("styled_map", styledMapType)
-      map.setMapTypeId("styled_map")
-    }
-
-    initMap()
-  }, [])
-
   return (
     <MapContainer>
       <Card>
@@ -57,7 +28,15 @@ const Map = ({ map }: MapProps): JSX.Element => {
         </AddressWrapper>
       </Card>
       <MapWrapper>
-        <div id="map" style={{ height: "100%" }} />
+        <GoogleMap
+          style={{ width: "100%", height: "100%" }}
+          styles={mapStyles}
+          defaultCenter={{ lat: 43.6214593381594, lng: 7.0392507747754625 }}
+          defaultZoom={17}
+          disableDefaultUI={true}
+        >
+          <Marker position={{ lat: 43.6214593381594, lng: 7.0392507747754625 }} />
+        </GoogleMap>
       </MapWrapper>
     </MapContainer>
   )
