57ff1bd9e901789b6eff1fbad9db7d735609c865
Anton Berezenskyi <ant.vla.ber@gmail.com>
2025-02-21 15:50:36 +0200

    remove 3rd party for google maps

--- src/layouts/ContactUs/Map/index.tsx
+++ src/layouts/ContactUs/Map/index.tsx
@@ -1,5 +1,5 @@
-import { Marker, Map as GoogleMap } from "@vis.gl/react-google-maps"
 import Image from "next/image"
+import { useLayoutEffect } from "react"
 
 import Card from "@components/Card"
 
@@ -15,6 +15,36 @@ interface MapProps {
 
 const Map = ({ map }: MapProps): JSX.Element => {
   const { title, description, image, address } = map
+
+  useLayoutEffect(() => {
+    let map
+    const google = (window as any)?.google
+
+    const initMap = async () => {
+      const position = { lat: 43.6214593381594, lng: 7.0392507747754625 }
+      const { Map } = await google.maps.importLibrary("maps")
+      const { AdvancedMarkerElement } = await google.maps.importLibrary("marker")
+
+      map = new Map(document.getElementById("map") as HTMLElement, {
+        zoom: 17,
+        center: position,
+        mapId: "contact-us-map",
+        disableDefaultUI: true,
+      })
+
+      new AdvancedMarkerElement({
+        map: map,
+        position: position,
+      })
+
+      const styledMapType = new google.maps.StyledMapType(mapStyles)
+      map.mapTypes.set("styled_map", styledMapType)
+      map.setMapTypeId("styled_map")
+    }
+
+    initMap()
+  }, [])
+
   return (
     <MapContainer>
       <Card>
@@ -27,15 +57,7 @@ const Map = ({ map }: MapProps): JSX.Element => {
         </AddressWrapper>
       </Card>
       <MapWrapper>
-        <GoogleMap
-          style={{ width: "100%", height: "100%" }}
-          styles={mapStyles}
-          defaultCenter={{ lat: 43.6214593381594, lng: 7.0392507747754625 }}
-          defaultZoom={17}
-          disableDefaultUI={true}
-        >
-          <Marker position={{ lat: 43.6214593381594, lng: 7.0392507747754625 }} />
-        </GoogleMap>
+        <div id="map" style={{ height: "100%" }} />
       </MapWrapper>
     </MapContainer>
   )
--- src/layouts/ContactUs/index.tsx
+++ src/layouts/ContactUs/index.tsx
@@ -1,7 +1,5 @@
 "use client"
 
-import { APIProvider } from "@vis.gl/react-google-maps"
-
 import Hero from "./Hero"
 import Map from "./Map"
 import { ContactUsYaml } from "./types"
@@ -14,10 +12,10 @@ const ContactUs = ({ yaml }: ContactUsProps): JSX.Element => {
   const { content } = yaml
 
   return (
-    <APIProvider apiKey={process.env.NEXT_PUBLIC_MAPS_SITE_KEY}>
+    <>
       <Hero hero={content.hero} />
       <Map map={content.map} />
-    </APIProvider>
+    </>
   )
 }
 
