0cfe2ee1b54df4e45354d8719315796194f1dc54
Anton Berezenskyi <ant.vla.ber@gmail.com>
2025-04-03 14:45:24 +0300

    developers and contributing pages additional changes

--- src/layouts/DocumentationIndex/index.tsx
+++ src/layouts/DocumentationIndex/index.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import GetStarted from "@components/GetStarted"
+
 import Hero from "./Hero"
 import ThumbList from "./ThumbList"
 import type { DocumentationIndexYaml } from "./types"
@@ -8,8 +10,8 @@ const DocumentationIndex = ({ yaml }: { yaml: DocumentationIndexYaml }): JSX.Ele
   return (
     <>
       <Hero {...yaml.content.hero} />
-
       <ThumbList thumbs={yaml.content.thumbs} />
+      <GetStarted getStarted={yaml.content.getStarted} />
     </>
   )
 }
