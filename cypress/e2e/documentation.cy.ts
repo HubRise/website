63ce4c6debb784155089418352fe7778b8b59f66
Anton Berezenskyi <ant.vla.ber@gmail.com>
2025-08-15 16:42:35 +0300

    change link params on apps page

--- cypress/e2e/documentation.cy.ts
+++ cypress/e2e/documentation.cy.ts
@@ -7,7 +7,7 @@ describe("Documentation", () => {
     it("switches language via header links", () => {
       cy.visit("/fr/apps/0test")
       cy.contains("Connecter Acme à HubRise")
-      languagesDropdown().invoke("show").children("a").contains("EN").click()
+      languagesDropdown().trigger("mouseover").find("li a").contains("EN").click()
       cy.contains("Connecting Acme to HubRise")
     })
 
