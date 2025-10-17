14ab899bc7346b179670ccb3c36055846fe48b03
antmonnier <monniera@gmail.com>
2025-10-17 19:28:36 +0200

    Prevent pre-rendering of robots.txt

--- src/app/robots.txt/route.ts
+++ src/app/robots.txt/route.ts
@@ -1,5 +1,8 @@
 import { NextResponse } from "next/server"
 
+// Prevents Next from pre-rendering this route at build time, as it relies on a runtime ENV variable
+export const dynamic = "force-dynamic"
+
 export async function GET(): Promise<Response> {
   // Block crawlers if NO_INDEX is set
   const content = process.env.NO_INDEX ? "User-agent: *\nDisallow: /\n" : "User-agent: *\nAllow: /\n"
